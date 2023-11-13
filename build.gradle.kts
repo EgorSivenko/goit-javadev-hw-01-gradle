61fa7bbf8db0789d41662ef1f9f05af4c088b550
EgorSivenko <egor.siv20@gmail.com>
2023-11-13 23:14:38 +0200

    Task Implementation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "com.example"
-version = "1.0"
+version = "1.1"
 
 repositories {
     mavenCentral()
@@ -12,6 +12,20 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    implementation("com.google.code.gson:gson:2.10.1")
+    compileOnly("org.projectlombok:lombok:1.18.26")
+    annotationProcessor("org.projectlombok:lombok:1.18.26")
+}
+
+val jar by tasks.getting(Jar::class) {
+    manifest {
+        attributes["Main-Class"] = "com.example.App"
+    }
+
+    from(configurations.runtimeClasspath
+            .get()
+            .files
+            .map { if (it.isDirectory) it else zipTree(it) })
 }
 
 tasks.test {
